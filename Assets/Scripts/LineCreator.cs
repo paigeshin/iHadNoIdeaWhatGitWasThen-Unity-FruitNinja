2ebb2a4cc9501e4cc9678c2a09f7b72a2b0302fa
shin <proscus0706@gmail.com>
2019-01-29 23:26:10 +0900

    fourth lecture

--- Assets/Scripts/LineCreator.cs
+++ Assets/Scripts/LineCreator.cs
@@ -26,52 +26,108 @@ public class LineCreator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Application.platform == RuntimePlatform.Android)
         {
-            //Input.GetMouseButtonDown(0) - This will be invoked when user clicks with the left mouse button.
-            mouseDown = true; //Allow to move to the next logic
-        }
-
-        if (mouseDown)
-        {
-            //Line Renderer logics begin here.
-
-            //line.SetVertexCount(vertexCount + 1); //When user clicks, increment the number of vertextCount. -  in other words, create lines.
-            line.positionCount = vertexCount + 1;
-
-            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Input.mousePosition returns the value of position. In other words, just get the value of mouse position
-
-
-            line.SetPosition(vertexCount, mousePos); //move the position of the line, according to the position of the mouse.
-            vertexCount++;
-
-            //Whenever the mouse is down, we will create the line and increment lines 
-            //and we are moving the position of the line according to the position of our mouse.
-
-            //Attach Box Collider to our line
-            BoxCollider2D box = gameObject.AddComponent<BoxCollider2D>(); //add box collider 2d
-            box.transform.position = line.transform.position; //change the position of the box collider to the position of the line.
-            box.size = new Vector2(0.1f, 0.1f); //just change the size.
-          
-        }
-
-        //we need to delete this mouse position and the line when the mouse button goes up.
-        if (Input.GetMouseButtonUp(0))
-        {
-            //Explanation about the method 'Input.GetMouseButtonUp(0)'. Which means mouse is unclicked.
-            mouseDown = false;
-            vertexCount = 0;
-
-            //line.SetVertexCount(0);
-            line.positionCount = 0; //Remove all the lines.
-
-            //Once BoxCollider is created, we need to destroy it.
-            BoxCollider2D[] colliders = GetComponents<BoxCollider2D>(); //this array will store all 'BoxCollider2D' 
-            foreach(BoxCollider2D box in colliders)
+            //If we run this application on Android
+            if(Input.touchCount > 0)
             {
-                Destroy(box); //Destroy them all!
+                //if there's at least one touch.
+                if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+                    //If finger touched on the screen and if it were moved,
+                    //then we need to check all the moves
+
+                    //Line Renderer logics begin here.
+
+                    //line.SetVertexCount(vertexCount + 1); //When user clicks, increment the number of vertextCount. -  in other words, create lines.
+                    line.positionCount = vertexCount + 1;
+
+                    //Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Input.mousePosition returns the value of position. In other words, just get the value of mouse position
+
+
+                    line.SetPosition(vertexCount, mousePos); //move the position of the line, according to the position of the mouse.
+                    vertexCount++;
+
+                    //Whenever the mouse is down, we will create the line and increment lines 
+                    //and we are moving the position of the line according to the position of our mouse.
+
+                    //Attach Box Collider to our line
+                    BoxCollider2D box = gameObject.AddComponent<BoxCollider2D>(); //add box collider 2d
+                    box.transform.position = line.transform.position; //change the position of the box collider to the position of the line.
+                    box.size = new Vector2(0.1f, 0.1f); //just change the size.
+                }
+
+                if(Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    //If user doesn't touch the screen..
+
+                    vertexCount = 0;
+
+                    //line.SetVertexCount(0);
+                    line.positionCount = 0; //Remove all the lines.
+
+                    //Once BoxCollider is created, we need to destroy it.
+                    BoxCollider2D[] colliders = GetComponents<BoxCollider2D>(); //this array will store all 'BoxCollider2D' 
+                    foreach (BoxCollider2D box in colliders)
+                    {
+                        Destroy(box); //Destroy them all!
+                    }
+                }
+
             }
         }
+
+        else if(Application.platform == RuntimePlatform.WindowsPlayer) { 
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Input.GetMouseButtonDown(0) - This will be invoked when user clicks with the left mouse button.
+                mouseDown = true; //Allow to move to the next logic
+            }
+
+            if (mouseDown)
+            {
+                //Line Renderer logics begin here.
+
+                //line.SetVertexCount(vertexCount + 1); //When user clicks, increment the number of vertextCount. -  in other words, create lines.
+                line.positionCount = vertexCount + 1;
+
+                //Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Input.mousePosition returns the value of position. In other words, just get the value of mouse position
+
+
+                line.SetPosition(vertexCount, mousePos); //move the position of the line, according to the position of the mouse.
+                vertexCount++;
+
+                //Whenever the mouse is down, we will create the line and increment lines 
+                //and we are moving the position of the line according to the position of our mouse.
+
+                //Attach Box Collider to our line
+                BoxCollider2D box = gameObject.AddComponent<BoxCollider2D>(); //add box collider 2d
+                box.transform.position = line.transform.position; //change the position of the box collider to the position of the line.
+                box.size = new Vector2(0.1f, 0.1f); //just change the size.
+          
+            }
+
+            //we need to delete this mouse position and the line when the mouse button goes up.
+            if (Input.GetMouseButtonUp(0))
+            {
+                //Explanation about the method 'Input.GetMouseButtonUp(0)'. Which means mouse is unclicked.
+                mouseDown = false;
+                vertexCount = 0;
+
+                //line.SetVertexCount(0);
+                line.positionCount = 0; //Remove all the lines.
+
+                //Once BoxCollider is created, we need to destroy it.
+                BoxCollider2D[] colliders = GetComponents<BoxCollider2D>(); //this array will store all 'BoxCollider2D' 
+                foreach(BoxCollider2D box in colliders)
+                {
+                    Destroy(box); //Destroy them all!
+                }
+            }
+        }
+
     }
 }
